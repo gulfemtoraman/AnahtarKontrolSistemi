4bf300125246103a16c5abc8bb4ea6c30ae5c096
Gülfem Toraman <43786748+gulfemtoraman@users.noreply.github.com>
2023-07-27 17:34:03 +0300

    Add files via upload

---
+++ Anahtar_Kontrol_Sistemi/Anahtar_Kontrol_Sistemi/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Anahtar_Kontrol_Sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.id_txt = new System.Windows.Forms.TextBox();
+            this.cvp_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RFID Tabanlı Anahtarlık Kontrol Sistemi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(15, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID numarası:";
+            // 
+            // id_txt
+            // 
+            this.id_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.id_txt.Location = new System.Drawing.Point(203, 93);
+            this.id_txt.Name = "id_txt";
+            this.id_txt.Size = new System.Drawing.Size(294, 38);
+            this.id_txt.TabIndex = 2;
+            this.id_txt.Text = "93    45    88    4";
+            // 
+            // cvp_lbl
+            // 
+            this.cvp_lbl.AutoSize = true;
+            this.cvp_lbl.Font = new System.Drawing.Font("Microsoft Tai Le", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cvp_lbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.cvp_lbl.Location = new System.Drawing.Point(11, 165);
+            this.cvp_lbl.Name = "cvp_lbl";
+            this.cvp_lbl.Size = new System.Drawing.Size(278, 56);
+            this.cvp_lbl.TabIndex = 3;
+            this.cvp_lbl.Text = "KAPI AÇILDI";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 253);
+            this.Controls.Add(this.cvp_lbl);
+            this.Controls.Add(this.id_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox id_txt;
+        private System.Windows.Forms.Label cvp_lbl;
+    }
+}
+
---
+++ Anahtar_Kontrol_Sistemi/Anahtar_Kontrol_Sistemi/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Anahtar_Kontrol_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+        }
+
+        private class serialPort1
+        {
+            public static string PortName { get; internal set; }
+            public static int BaudRate { get; internal set; }
+
+            internal static void Open()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
